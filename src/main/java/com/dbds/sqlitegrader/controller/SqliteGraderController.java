a9fa751af5bcfff936728c96b9dc2f9fc7adf372
ketan1729 <103006434+ketan1729@users.noreply.github.com>
2022-04-09 23:16:06 -0400

    Add files via upload

---
+++ src/main/java/com/dbds/sqlitegrader/controller/SqliteGraderController.java
@@ -0,0 +1,12 @@
+package com.dbds.sqlitegrader.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/sqlite")
+public class SqliteGraderController {
+	
+}
---
+++ src/test/java/com/dbds/instacart/SqliteGraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbds.instacart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqliteGraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
